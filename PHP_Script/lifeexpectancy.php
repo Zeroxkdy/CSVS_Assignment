abcfb3352c0a98fa9ee7b66fbdbee3e143d7244f
Zerox <193876930+Zeroxkdy@users.noreply.github.com>
2025-07-30 12:08:37 +0800

    Update lifeexpectancy.php

--- PHP_Script/lifeexpectancy.php
+++ PHP_Script/lifeexpectancy.php
@@ -1,36 +1,27 @@
-<a href="query.php">Pick another query</a>
- 
- <?php
-        //SQL to get birthrate and life expectancy    
-        $sql = "select name, birthrate, lifeexpectancy from countrydata_final;";
-        
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            //echo "success";
-            echo '<table style="width: 100%">';
+<div class="container py-4">
+    <a href="query.php" class="btn btn-outline-primary mb-3">⬅ Pick Another Query</a>
+    <h3 class="mb-4">Birth Rate and Life Expectancy by Country</h3>
+
+    <?php
+    $sql = "SELECT name, birthrate, lifeexpectancy FROM countrydata_final;";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo '<div class="table-responsive">';
+        echo '<table class="table table-striped table-bordered">';
+        echo '<thead><tr><th>Country Name</th><th>Birth Rate</th><th>Life Expectancy</th></tr></thead>';
+        echo '<tbody>';
+        while ($row = $result->fetch_assoc()) {
             echo '<tr>';
-            echo '<th style="text-align:left">Country Name</th>';
-            echo '<th style="text-align:left">Birth Rate</th>';
-            
-            echo '<th style="text-align:left">Life Expectancy</th>';
+            echo '<td>' . htmlspecialchars($row["name"]) . '</td>';
+            echo '<td>' . htmlspecialchars($row["birthrate"]) . '</td>';
+            echo '<td>' . htmlspecialchars($row["lifeexpectancy"]) . '</td>';
             echo '</tr>';
-            
-            while($row = $result->fetch_assoc()) {
-            //echo '<p>';
-           
-            echo '<tr>';
-            echo '<td>';
-            echo $row["name"];
-            echo '&nbsp';
-            echo '<td>';
-            echo $row["birthrate"];
-            echo '&nbsp';
-            echo '<td>';
-            echo $row["lifeexpectancy"];
-            echo '&nbsp';
-            echo '<br';
-            echo '</tr>';
-            }
-            echo '</table>';
         }
+        echo '</tbody></table>';
+        echo '</div>';
+    } else {
+        echo '<div class="alert alert-warning">No data found.</div>';
+    }
     ?>
+</div>
